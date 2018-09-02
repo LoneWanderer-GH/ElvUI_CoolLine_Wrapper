820940e0b6deee5ec0430881dca42b0237e8e083
LoneWanderer-GH <LoneWanderer-GH@users.noreply.github.com>
2018-09-03 01:19:48 +0200

    Implementation updates
    - Put some more generic code to handle various addons and their frames (provided we can find the frame to handle ...)
    - Revamped options part, however this is not very useful yet. Some keys could be stored better with an option hierarchy instead of all flat with generated key ;) (code related to Options is disabled things it does not really work)
    
    Known issue(s):
    - does not intercept the TomTom addon arrow frame. Maybe linked to the way things appear in the frame (late creation by TomTom addon ?)
    
    Still to do:
    - more addon options encapsulation ?

--- ElvUI_CoolLine_Wrapper.lua
+++ ElvUI_CoolLine_Wrapper.lua
@@ -3,23 +3,28 @@ local E, L, V, P, G = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, Profi
 local ElvUI_CoolLine_Wrapper = E:NewModule('ElvUI_CoolLine_Wrapper', 'AceHook-3.0', 'AceEvent-3.0', 'AceTimer-3.0'); --Create a plugin within ElvUI and adopt AceHook-3.0, AceEvent-3.0 and AceTimer-3.0. We can make use of these later.
 local EP = LibStub("LibElvUIPlugin-1.0") --We can use this to automatically insert our GUI tables when ElvUI_Config is loaded.
 local addonName, addonTable = ... --See http://www.wowinterface.com/forums/showthread.php?t=51502&p=304704&postcount=2
+--local E_Conf = P[addonName]
+--P["ElvUI_CoolLine_Wrapper"] = {
+--    ["FrameMoverToggleOption"] = false,
+--}
+P[addonName] = {}
 
-P["ElvUI_CoolLine_Wrapper"] = {
-    ["FrameMoverToggleOption"] = false,
-}
-
-local nbCreated = 0
-local WrapperCreatedMovers = {}
+--local nbCreated = 0
+--local WrapperCreatedMovers = {}
 
 local IsAddOnLoaded = IsAddOnLoaded
 
--- preparation of a more Generic version of this addon to create movers for "any" addon ... to be continued/confirmed
-local wrappedAddonName = "CoolLine"
+-- association Addon <=> Frame
+local wrappedAddons = {
+    ["CoolLine"] = {{frame = CoolLine, description = "The CoolLine cooldowns frame"}, },
+    ["TomTom"] = {{frame = TomTomCrazyArrow, description = "The TomTom arrow frame"}, },
+}
 
---local wrappedAddons = {
---    "AddonName" = "FrameName",
---    -- todo: think of way to store list of frames and wrap them
---    "CoolLine" = "CoolLine"
+---- association Addon <=> Framewdescription (in case several frames to manage)
+--local wrappedAddonsFramesDesriptions = {
+--    --"AddonName" = "FrameObject",
+--    ["CoolLine"] = "CoolLine",
+--    ["TomTom"] = "TomTomCrazyArrow",
 --}
 
 -----------------------------------------
@@ -27,139 +32,192 @@ local function MyLogger(methodName, text)
     -----------------------------------------
     print ("|cff00b3ff" .. addonName .. " - " .. methodName .. "()|r - " .. text)
 end
-
+local function buildMoverName(wrappedAddonName, frameDescription)
+    return wrappedAddonName .. "__" .. frameDescription .. "__Mover"
+end
 -----------------------------------------
-local function Create_ElvUI_Mover(frame, stringName)
+local function Create_ElvUI_Mover(wrappedAddonName, frame, frameDescription)
     -----------------------------------------
-    local MoverUniqueName = wrappedAddonName .. "__" .. stringName .. "__Mover"
-    local MoverDisplayName = "E_WrapMover_"..wrappedAddonName
-
+    local MoverUniqueName = buildMoverName(wrappedAddonName, frameDescription)
     if not E.CreatedMovers[MoverUniqueName] then
-
-        MyLogger("Create_ElvUI_Mover", "Mover for " .. stringName .. " not yet created")
-
+        --MyLogger("Create_ElvUI_Mover", "Mover for " .. frameDescription .. " not yet created")
         if frame then
+            --MyLogger("Create_ElvUI_Mover", "Frame " .. frameDescription .. " found. Create mover")
+            E:CreateMover(frame, MoverUniqueName, MoverUniqueName, nil, nil, nil, 'ALL,SOLO,ACTIONSBARS,PARTY,ARENA,RAID')
 
-            MyLogger("Create_ElvUI_Mover", "Frame " .. stringName .. " found. Create mover")
-
-            nbCreated = nbCreated + 1
-            MoverDisplayName = MoverDisplayName .. "_" .. nbCreated
-
-            E:CreateMover(frame, MoverUniqueName, MoverDisplayName, nil, nil, nil, 'ALL,SOLO,ACTIONSBARS,PARTY,ARENA,RAID')
-
-            MyLogger("Create_ElvUI_Mover", MoverDisplayName .. " mover created")
-
-            WrapperCreatedMovers [nbCreated] = MoverDisplayName
         else
-            MyLogger("Create_ElvUI_Mover", "Frame " .. stringName .. " not found")
+            -- TODO : put some code to perform a retry ?
+            MyLogger("Create_ElvUI_Mover", "Frame " .. frameDescription .. " not found")
         end
     else
-        MyLogger("Create_ElvUI_Mover", MoverDisplayName .. " already exists")
+        --MyLogger("Create_ElvUI_Mover", MoverDisplayName .. " already exists")
+    end
+end
+
+local function buildOptionKey(wrappedAddonName, frameDescription)
+    return wrappedAddonName .. "_" .. frameDescription .. "_ToggleOption"
+end
+
+local function addOptionKey(wrappedAddonName, frameDescription, value)
+    --if E_Conf then
+    if P then
+        if P[addonName] then
+            P[addonName][buildOptionKey(wrappedAddonName, frameDescription)] = value
+        else
+            MyLogger("addOptionKey", "ElvUI profileDB has no value for key " .. addonName .. " ?")
+        end
+    else
+        MyLogger("addOptionKey", "ElvUI profileDB does not exist ?")
     end
 end
 
 -----------------------------------------
 function ElvUI_CoolLine_Wrapper:CreateMovers()
     -----------------------------------------
-    Create_ElvUI_Mover(CoolLine, "CoolLine")
+    for wrappedAddonName, wrappedAddonFramesData in pairs(wrappedAddons) do
+        for k1, frameData in pairs(wrappedAddonFramesData) do
+            local frame = frameData.frame
+            local frameDescription = frameData.description
+
+            --MyLogger("CreateMovers", "processing ".. wrappedAddonName .." conf")
+            --MyLogger("CreateMovers", wrappedAddonName .." - " .. frame .. " - " .. frameDescription)
+
+            if IsAddOnLoaded(wrappedAddonName) then
+                --MyLogger("CreateMovers", wrappedAddonName .. " is loaded, create the mover")
+                Create_ElvUI_Mover(wrappedAddonName, frame, frameDescription)
+                addOptionKey(wrappedAddonName, frameDescription, true)
+            else
+                MyLogger("CreateMovers", wrappedAddonName .. " not yet loaded ?! Addon probably absent.")
+            end
+        end
+    end
 end
 
 -----------------------------------------
 function ElvUI_CoolLine_Wrapper:Update()
     -----------------------------------------
-    -- call when config has changed ?!
     MyLogger("Update", "Start")
-    local enabled = E.db.ElvUI_CoolLine_Wrapper.FrameMoverToggleOption
 
-    --MyLogger("Update", "Try to create Mover")
-
-    --self:CreateMovers()
-
-    for k, v in pairs(WrapperCreatedMovers) do
-        if E.CreatedMovers[v] then
-
-            MyLogger("Update", "Mover " .. v .. " exists")
-
-            if enabled then
-
-                MyLogger("Update", "Enable " .. v)
-                E:EnableMover(v)
-
+    for wrappedAddonName, wrappedAddonFramesData in pairs(wrappedAddons) do
+        for k1, frameData in pairs(wrappedAddonFramesData) do
+            local frame = frameData.frame
+            local frameDescription = frameData.description
+            local optionKey = buildOptionKey(wrappedAddonName, frameDescription)
+            local MoverUniqueName = buildMoverName(wrappedAddonName, frameDescription)
+            local enabled = E.db.ElvUI_CoolLine_Wrapper[optionKey]
+            if E.CreatedMovers[MoverUniqueName] then
+                if enabled then
+                    MyLogger("Update", "Enable " .. optionKey)
+                    E:EnableMover(MoverUniqueName)
+                else
+                    MyLogger("Update", "Disable " .. optionKey)
+                    E:DisableMover(MoverUniqueName)
+                end
             else
-
-                MyLogger("Update", "Disable " .. v)
-                E:DisableMover(v)
-
+                MyLogger("Update", "Mover "..MoverUniqueName.." supposed to exist but not found")
             end
-        else
-            MyLogger("Update", "Mover " .. v .. " supposed to already be created but does not exist yet ?!")
         end
     end
-
 end
 
+local function addToggleConfigOption(wrappedAddonName, frame, frameDescription, optionKey, frameOptionOrder)
+    E.Options.args[addonName].args[wrappedAddonName].args[frameDescription] = {
+        order = frameOptionOrder,
+        type = "toggle",
+        name = frameDescription,
+        get = function(info)
+            return E.db.ElvUI_CoolLine_Wrapper[optionKey]
+        end,
+        set = function(info, value)
+            MyLogger(wrappedAddonName .. " ToggleOptionChanged", frameDescription .. " ToggleOption ")
+            E.db.ElvUI_CoolLine_Wrapper[optionKey] = value
+            ElvUI_CoolLine_Wrapper:Update()
+        end,
+        --frame = frame
+    }
+end
+local function addWrapperAddonOptions(wrappedAddonName, wrappedAddonFramesData)
+    -- fill addon wrapping options
+    local frameOptionOrder = 1
+    for k1, frameData in pairs(wrappedAddonFramesData) do
+        local frame = frameData.frame
+        local frameDescription = frameData.description
+        local optionKey = buildOptionKey(wrappedAddonName, frameDescription)
+
+        --MyLogger("addWrapperAddonOptions", "Treating " .. wrappedAddonName .. " frame " .. frameDescription)
+        --MyLogger("addWrapperAddonOptions", "Add correspongind toggle option")
+
+        addToggleConfigOption(wrappedAddonName, frame, frameDescription, optionKey, frameOptionOrder)
+        frameOptionOrder = frameOptionOrder + 1
+    end
+end
 -----------------------------------------
 function ElvUI_CoolLine_Wrapper:InsertOptions()
     -----------------------------------------
-    MyLogger("InsertOptions", "Start")
+    ----MyLogger("InsertOptions", "Start")
+    ----MyLogger("InsertOptions", "Add " .. addonName .. "group")
+    --E.Options.args[addonName] = {
+    --    order = 5,
+    --    type = "group",
+    --    name = "|cFF00FFFFAddonFrames|ralpha",
+    --    args = {
+    --        generalHeader = {
+    --            order = 1,
+    --            type = "header",
+    --            name = addonName,
+    --        },
+    --    },
+    --}
 
-    E.Options.args.ElvUI_CoolLine_Wrapper = {
-        order = 5,
-        type = "group",
-        name = "> " .. wrappedAddonName .. " <",
-        args = {
-            FrameMoverToggleOption = {
-                order = 1,
-                type = "toggle",
-                name = wrappedAddonName .. " Frames Movers",
-                get = function(info)
-                    return E.db.ElvUI_CoolLine_Wrapper.FrameMoverToggleOption
-                end,
-                set = function(info, value)
-                    MyLogger("ToggleOptionChanged", "FrameMoverToggleOption")
-                    E.db.ElvUI_CoolLine_Wrapper.FrameMoverToggleOption = value
-                    ElvUI_CoolLine_Wrapper:Update()
-                end,
-            },
-        },
-    }
+    ----MyLogger("InsertOptions", "Start loop over all wrapped addons")
+    --for wrappedAddonName, wrappedAddonFramesData in pairs(wrappedAddons) do
+    --    local optionsOrder = 1
+    --    --MyLogger("InsertOptions", "Treating " .. wrappedAddonName)
+    --    --MyLogger("InsertOptions", "Add generalHeader")
+    --    E.Options.args[addonName].args[wrappedAddonName] = {
+    --        order = optionsOrder,
+    --        type = "group",
+    --        name = "|cff1784d1" .. wrappedAddonName,
+    --        args = {},
+    --        --toggleOptions = {},
+    --    }
+
+    --    --MyLogger("InsertOptions", "Start loop over " .. wrappedAddonName .." managed frames")
+    --    addWrapperAddonOptions(wrappedAddonName, wrappedAddonFramesData)
+
+    --    optionsOrder = optionsOrder + 1
+    --end
+    --MyLogger("InsertOptions", "End !")
 end
 
 -----------------------------------------
 function ElvUI_CoolLine_Wrapper:Initialize()
     -----------------------------------------
-    MyLogger("Initialize", "Start")
-
-    MyLogger("Initialize", "called from ElvUI because of previous register")
-
+    --MyLogger("Initialize", "Start")
+    --MyLogger("Initialize", "called from ElvUI because of previous register")
     EP:RegisterPlugin(addonName, ElvUI_CoolLine_Wrapper.InsertOptions)
-
-    if IsAddOnLoaded(wrappedAddonName) then
-        MyLogger("Initialize", wrappedAddonName .. " is loaded, create the mover")
-        self:CreateMovers()
-    else
-        MyLogger("Initialize", wrappedAddonName .. " not yet loaded ?! Addon probably absent.")
-    end
+    self:CreateMovers()
+    --self:InsertOptions()
 end
 
 --------------------------------
 function ElvUI_CoolLine_Wrapper:doRegister()
     --------------------------------
-    MyLogger("doRegister", "Register module to ELVUI. This will later trigger the Initialize method")
+    --MyLogger("doRegister", "Register module to ELVUI. This will later trigger the Initialize method")
     E:RegisterModule(ElvUI_CoolLine_Wrapper:GetName())
 end
 
 --------------------------------
 function ElvUI_CoolLine_Wrapper:PLAYER_ALIVE()
     --------------------------------
-    MyLogger("PLAYER_ALIVE", "Start")
+    --MyLogger("PLAYER_ALIVE", "Start")
     self:doRegister()
 end
 
 --------------------------------
 function ElvUI_CoolLine_Wrapper:PLAYER_DEAD()
     --------------------------------
-    MyLogger("PLAYER_DEAD", "Start")
+    --MyLogger("PLAYER_DEAD", "Start")
     self:doRegister()
 end
 
