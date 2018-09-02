f71c15533103be800860e87e3ca7673ce7b11670
LoneWanderer-GH <LoneWanderer-GH@users.noreply.github.com>
2018-09-02 21:01:23 +0200

    Fix Movers Creation
    - Registerer addon into ElvUI when WoW events PLAYER_ALIVE PLAYER_DEAD triggered
    - At register time, try to create the movers
    - several fixes (fancy console print logger)

--- ElvUI_CoolLine_Wrapper.lua
+++ ElvUI_CoolLine_Wrapper.lua
@@ -4,82 +4,165 @@ local ElvUI_CoolLine_Wrapper = E:NewModule('ElvUI_CoolLine_Wrapper', 'AceHook-3.
 local EP = LibStub("LibElvUIPlugin-1.0") --We can use this to automatically insert our GUI tables when ElvUI_Config is loaded.
 local addonName, addonTable = ... --See http://www.wowinterface.com/forums/showthread.php?t=51502&p=304704&postcount=2
 
-local MoverName = "ElvUI_CoolLine_Mover"
-
 P["ElvUI_CoolLine_Wrapper"] = {
-	["FrameMoverToggleOption"] = false,
+    ["FrameMoverToggleOption"] = false,
 }
 
+local nbCreated = 0
+local WrapperCreatedMovers = {}
+
+local IsAddOnLoaded = IsAddOnLoaded
+
+-- preparation of a more Generic version of this addon to create movers for "any" addon ... to be continued/confirmed
+local wrappedAddonName = "CoolLine"
+
+local wrappedAddons = {
+    "AddonName" = "FrameName"
+    -- todo: think of way to store list of frames and wrap them
+    "CoolLine" = "CoolLine"
+}
+
+-----------------------------------------
+local function MyLogger(methodName, text)
+    -----------------------------------------
+    print ("|cff00b3ff" .. addonName .. " - " .. methodName .. "()|r - " .. text)
+end
+
+-----------------------------------------
+local function Create_ElvUI_Mover(frame, stringName)
+    -----------------------------------------
+    local MoverUniqueName = wrappedAddonName .. "__" .. stringName .. "__Mover"
+    local MoverDisplayName = "E_WrapMover_"..wrappedAddonName
+
+    if not E.CreatedMovers[MoverUniqueName] then
+
+        MyLogger("Create_ElvUI_Mover", "Mover for " .. stringName .. " not yet created")
+
+        if frame then
+
+            MyLogger("Create_ElvUI_Mover", "Frame " .. stringName .. " found. Create mover")
+
+            nbCreated = nbCreated + 1
+            MoverDisplayName = MoverDisplayName .. "_" .. nbCreated
+
+            E:CreateMover(frame, MoverUniqueName, MoverDisplayName, nil, nil, nil, 'ALL,SOLO,ACTIONSBARS,PARTY,ARENA,RAID')
+
+            MyLogger("Create_ElvUI_Mover", MoverDisplayName .. " mover created")
+
+            WrapperCreatedMovers [nbCreated] = MoverDisplayName
+        else
+            MyLogger("Create_ElvUI_Mover", "Frame " .. stringName .. " not found")
+        end
+    else
+        MyLogger("Create_ElvUI_Mover", MoverDisplayName .. " already exists")
+    end
+end
+
+-----------------------------------------
+function ElvUI_CoolLine_Wrapper:CreateMovers()
+    -----------------------------------------
+    Create_ElvUI_Mover(CoolLine, "CoolLine")
+end
+
+-----------------------------------------
 function ElvUI_CoolLine_Wrapper:Update()
-	local enabled = E.db.ElvUI_CoolLine_Wrapper.FrameMoverToggleOption
-    
-    if not E.CreatedMovers[MoverName] then
-        if CoolLine.MainFrame then
-            E:CreateMover(CoolLine.MainFrame, MoverName, L[MoverName], nil, nil, nil, 'ALL,SOLO,ACTIONSBARS,PARTY,ARENA,RAID')
-            print ('Update-Mover created')
+    -----------------------------------------
+    -- call when config has changed ?!
+    MyLogger("Update", "Start")
+    local enabled = E.db.ElvUI_CoolLine_Wrapper.FrameMoverToggleOption
+
+    --MyLogger("Update", "Try to create Mover")
+
+    --self:CreateMovers()
+
+    for k, v in pairs(WrapperCreatedMovers) do
+        if E.CreatedMovers[v] then
+
+            MyLogger("Update", "Mover " .. v .. " exists")
+
+            if enabled then
+
+                MyLogger("Update", "Enable " .. v)
+                E:EnableMover(v)
+
+            else
+
+                MyLogger("Update", "Disable " .. v)
+                E:DisableMover(v)
+
+            end
         else
-            -- find a way to postpone creation later ?
-            print ('Update-Find a way to postpone creation later ?')
+            MyLogger("Update", "Mover " .. v .. " supposed to already be created but does not exist yet ?!")
         end
     end
-    if E.CreatedMovers[MoverName] then
-        print ('Update-Mover exists')
-        if enabled then
-            print ('Update-Enable it')
-            E:EnableMover(MoverName)
-        else
-            print ('Update-Disable it')
-            E:DisableMover(MoverName)
-        end
-    else
-        print ('Update-Mover does not exist yet')
-    end
+
 end
 
+-----------------------------------------
 function ElvUI_CoolLine_Wrapper:InsertOptions()
-	E.Options.args.ElvUI_CoolLine_Wrapper = {
-		order = 5,
-		type = "group",
-		name = "> CoolLine <",
-		args = {
-			FrameMoverToggleOption = {
-				order = 1,
-				type = "toggle",
-				name = "CoolLine Frame Mover",
-				get = function(info)
-					return E.db.ElvUI_CoolLine_Wrapper.FrameMoverToggleOption
-				end,
-				set = function(info, value)
-                    print ('OptionChanged-FrameMoverToggleOption')
-					E.db.ElvUI_CoolLine_Wrapper.FrameMoverToggleOption = value
-					ElvUI_CoolLine_Wrapper:Update()
-				end,
-			},
-		},
-	}
+    -----------------------------------------
+    MyLogger("InsertOptions", "Start")
+
+    E.Options.args.ElvUI_CoolLine_Wrapper = {
+        order = 5,
+        type = "group",
+        name = "> " .. wrappedAddonName .. " <",
+        args = {
+            FrameMoverToggleOption = {
+                order = 1,
+                type = "toggle",
+                name = wrappedAddonName .. " Frames Movers",
+                get = function(info)
+                    return E.db.ElvUI_CoolLine_Wrapper.FrameMoverToggleOption
+                end,
+                set = function(info, value)
+                    MyLogger("ToggleOptionChanged", "FrameMoverToggleOption")
+                    E.db.ElvUI_CoolLine_Wrapper.FrameMoverToggleOption = value
+                    ElvUI_CoolLine_Wrapper:Update()
+                end,
+            },
+        },
+    }
 end
 
+-----------------------------------------
 function ElvUI_CoolLine_Wrapper:Initialize()
-    print ('Initialize')
-	EP:RegisterPlugin(addonName, ElvUI_CoolLine_Wrapper.InsertOptions)
-    ElvUI_CoolLine_Wrapper:Update()
-end
+    -----------------------------------------
+    MyLogger("Initialize", "Start")
 
-ElvUI_CoolLine_Wrapper:RegisterEvent("ADDON_LOADED")
-function ElvUI_CoolLine_Wrapper:ADDON_LOADED(a1)
-	if a1 ~= "CoolLine" then return end
-	self:UnregisterEvent("ADDON_LOADED")
-    if not E.CreatedMovers[MoverName] then
-        if CoolLine.MainFrame then
-            E:CreateMover(CoolLine.MainFrame, MoverName, L[MoverName], nil, nil, nil, 'ALL,SOLO,ACTIONSBARS,PARTY,ARENA,RAID')
-            print ('ADDON_LOADED-Mover created')
-        else
-            -- find a way to postpone creation later ?
-            print ('ADDON_LOADED-Find a way to postpone creation later ?')
-        end
+    MyLogger("Initialize", "called from ElvUI because of previous register")
+
+    EP:RegisterPlugin(addonName, ElvUI_CoolLine_Wrapper.InsertOptions)
+
+    if IsAddOnLoaded(wrappedAddonName) then
+        MyLogger("Initialize", wrappedAddonName .. " is loaded, create the mover")
+        self:CreateMovers()
     else
-        print ('ADDON_LOADED-Mover exists')
+        MyLogger("Initialize", wrappedAddonName .. " not yet loaded ?! Addon probably absent.")
     end
 end
 
-E:RegisterModule(ElvUI_CoolLine_Wrapper:GetName())
+--------------------------------
+function ElvUI_CoolLine_Wrapper:doRegister()
+    --------------------------------
+    MyLogger("doRegister", "Register module to ELVUI. This will later trigger the Initialize method")
+    E:RegisterModule(ElvUI_CoolLine_Wrapper:GetName())
+end
+
+--------------------------------
+function ElvUI_CoolLine_Wrapper:PLAYER_ALIVE()
+    --------------------------------
+    MyLogger("PLAYER_ALIVE", "Start")
+    self:doRegister()
+end
+
+--------------------------------
+function ElvUI_CoolLine_Wrapper:PLAYER_DEAD()
+    --------------------------------
+    MyLogger("PLAYER_DEAD", "Start")
+    self:doRegister()
+end
+
+-- addon reacts only to these events
+ElvUI_CoolLine_Wrapper:RegisterEvent("PLAYER_ALIVE")
+ElvUI_CoolLine_Wrapper:RegisterEvent("PLAYER_DEAD")
